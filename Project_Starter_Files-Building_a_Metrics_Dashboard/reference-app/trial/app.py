9081e6cb583c83912a1119e46d5e27c3e31c06fd
Jubair <zub1984.kn@gmail.com>
2021-11-04 17:33:26 +0300

    trial build

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/trial/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/trial/app.py
@@ -50,7 +50,7 @@ def init_tracer(service):
     # this call also sets opentracing.tracer
     return config.initialize_tracer()
 
-tracer = init_tracer('first-service')
+tracer = init_tracer('trial-service')
 
 @app.route('/')
 def homepage():
